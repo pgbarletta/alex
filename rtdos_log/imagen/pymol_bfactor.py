58fe04e828570fb064bc82d6d4e3df01c7375c52
pbarletta <pbarletta@gmail.com>
2018-01-09 17:51:43 -0600

    Done w/ all plots. Added protein pic, colored by RMSF.

---
+++ rtdos_log/imagen/pymol_bfactor.py
@@ -0,0 +1,24 @@
+from pymol import cmd
+
+cmd.load("./1r.pdb")
+cmd.hide("lines")
+cmd.show("cartoon")
+cmd.set("cartoon_fancy_helices", 1)
+cmd.set("ray_trace_mode",  1)
+cmd.set("two_sided_lighting", "on")
+cmd.set("reflect", 0)
+cmd.set("ambient", 0.5)
+cmd.set("ray_trace_mode",  0)
+cmd.set('''ray_opaque_background''', '''off''')
+
+inFile = open("rmsf_pymol", 'r')
+stored.newB = []
+for line in inFile.readlines(): stored.newB.append( float(line) )
+inFile.close()
+alter 1r_starting, b=0.0
+alter 1r_starting and n. CA, b=stored.newB.pop(0)
+
+# color the protein based on the new B Factors of the alpha carbons
+cmd.spectrum("b", "1r_starting and n. CA")
+
+cmd.png("bfac_1r.png", width=1800, height=1100, dpi=600, ray=1)
