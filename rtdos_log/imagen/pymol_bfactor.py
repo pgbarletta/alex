20edb228f6f36c3dc0449ef240d519a67615903a
pbarletta <pbarletta@gmail.com>
2018-01-26 12:49:47 -0600

    Done w/ some images

--- rtdos_log/imagen/pymol_bfactor.py
+++ rtdos_log/imagen/pymol_bfactor.py
@@ -1,6 +1,6 @@
 from pymol import cmd
 
-cmd.load("./1r.pdb")
+cmd.load("1r_final.pdb")
 cmd.hide("lines")
 cmd.show("cartoon")
 cmd.set("cartoon_fancy_helices", 1)
@@ -15,10 +15,10 @@ inFile = open("rmsf_pymol", 'r')
 stored.newB = []
 for line in inFile.readlines(): stored.newB.append( float(line) )
 inFile.close()
-alter 1r_starting, b=0.0
-alter 1r_starting and n. CA, b=stored.newB.pop(0)
+alter 1r_final, b=0.0
+alter 1r_final and n. CA, b=stored.newB.pop(0)
 
 # color the protein based on the new B Factors of the alpha carbons
-cmd.spectrum("b", "1r_starting and n. CA")
+cmd.spectrum("b", "1r_final and n. CA")
 
-cmd.png("bfac_1r.png", width=1800, height=1100, dpi=600, ray=1)
+cmd.png("bfac_1r_final.png", width=1800, height=1100, dpi=600, ray=1)
